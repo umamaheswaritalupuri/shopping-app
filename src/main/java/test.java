4ed4494d96d2676e49aa5bfac5d1186a3809912f
umamaheswaritalupuri <umac0511.devops@gmail.com>
2026-06-26 15:38:57 +0530

    Add Hello World example in App class

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,1 +1,11 @@
-jdbksdj
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(sayHello("World"));
+    }
+
+    public static String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+}
